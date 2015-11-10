e762b6d3c1e439217842088572bfcd7710750d52
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-10 12:08:53 -0700

    api endpoint added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/api/vi/guitars', function (req, res) {
+  res.json([
+    {id:1, make:'Martin', model:'D15'},
+    {id: 2, make:'Fender', model:'Stratocaster'}
+  ])
+});
+
 router.get('*', function(req, res, next) {
   res.sendFile('index.html', {
     root: __dirname + '/../public'
