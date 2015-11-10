2af55b049e538de930d6809caa119b432ec0beb4
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-10 16:31:40 -0700

    search functionality

--- public/app.js
+++ public/app.js
@@ -1,24 +1,6 @@
     angular.module('MyApp', ['ngRoute'])
       .controller('HomeController', function($scope, $http) {
-        // $http.get('https://api.github.com/zen').then(function(response){
-        //   $scope.zenData = response.data;
-        // });
-        // $http.get('https://still-tundra-8387.herokuapp.com/messages').then(function(response){
-        //   $scope.messages = response.data;
-        // });
-        // $scope.submit = function () {
-        //   var dataObj = {
-        //     name : $scope.name,
-        //     content : $scope.message,
-        //   };  
-        //   $http.post('https://still-tundra-8387.herokuapp.com/messages', {message: dataObj}).then(function(response){
-        //       console.log(response.data)
-        //   }, function(){
-        //       console.log('error');
-        //   });
-        //   $scope.name='';
-        //   $scope.message='';
-        // }
+
       })
       .controller('ChatController', function ($scope, $http) {
         $http.get('https://api.github.com/zen').then(function(response){
@@ -39,6 +21,13 @@
           });
           $scope.name='';
           $scope.message='';
+        }
+      })
+      .controller("MovieController", function ($scope, $http) {
+        $scope.search = function() {
+          $http.get('http://www.omdbapi.com/?s=' + $scope.title.toLowerCase() + '&r=json').then(function (response) {
+            $scope.movies = response.data.Search;
+          });
         }
       })
       .config(function($routeProvider, $locationProvider) {
